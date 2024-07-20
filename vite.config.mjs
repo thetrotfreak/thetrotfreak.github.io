b28e9f53ebed883e68a8bd0949fb41c3b4365eb4
thetrotfreak <thetrotfreak@yahoo.com>
2024-07-20 18:12:14 +0530

    build: update vite build dir

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/thetrotfreak.github.io/", // Replace with your GitHub repository name
+  base: "/thetrotfreak.github.io/",
+  build: {
+    outDir: "build"
+  }
 });
